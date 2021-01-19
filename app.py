ed153dec4b643916c01a3b786139d610a5fbdd4f
Iain Muir <iam9ez@virginia.edu>
2021-01-19 14:57:04 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+"""
+Iain Muir, iam9ez
+
+PROJECT DESCRIPTION
+
+streamlit run /Users/iainmuir/PycharmProjects/Desktop/sportsHighlights/app.py
+"""
+
+import streamlit as st
+
+import home
+import highlights
+import schedule
+import analytics
+
+PAGES = {
+    "Home": home,
+    "Highlights": highlights,
+    "Schedule": schedule,
+    "Analytics": analytics,
+}
+
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.run()
---
+++ schedule.py
@@ -0,0 +1,12 @@
+# Iain Muir
+# iam9ez
+
+import streamlit as st
+
+
+def run():
+    st.write(__name__)
+
+
+if __name__ == '__main__':
+    run()
