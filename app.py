a74496c9bce073b243484f5e7f8e7de621e44b54
Iain Muir <iam9ez@virginia.edu>
2021-01-21 18:40:40 -0500

    launch functions

--- app.py
+++ app.py
@@ -8,21 +8,18 @@ streamlit run /Users/iainmuir/PycharmProjects/Desktop/sportsHighlights/app.py
 
 import streamlit as st
 
-import home
-import highlights
-import schedule
-import analytics
+from sportsHighlights import home, highlights, schedule, analytics
 
-# st.set_page_config(layout="wide")
 
-PAGES = {
-    "Home": home,
-    "Highlights": highlights,
-    "Schedule": schedule,
-    "Analytics": analytics,
-}
+def launch():
+    PAGES = {
+        "Home": home,
+        "Highlights": highlights,
+        "Schedule": schedule,
+        "Analytics": analytics,
+    }
 
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-page = PAGES[selection]
-page.run()
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+    page = PAGES[selection]
+    page.run()
